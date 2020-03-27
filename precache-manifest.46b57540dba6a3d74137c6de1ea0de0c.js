d6e2d2167dcce0db3d3c4a0aacebb3d773feffd4
TheZano <fauzan.habib@outlook.com>
2020-03-27 13:49:27 +0700

    Updates

---
+++ precache-manifest.46b57540dba6a3d74137c6de1ea0de0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c68e433923e2b035f81c7b680087dd",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "d4d807041bec68aa6d1d",
+    "url": "/moviedb/static/css/main.c90f3fe3.chunk.css"
+  },
+  {
+    "revision": "624dd87a7164408d5107",
+    "url": "/moviedb/static/js/2.890ad9cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb/static/js/2.890ad9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d807041bec68aa6d1d",
+    "url": "/moviedb/static/js/main.58a4b30f.chunk.js"
+  },
+  {
+    "revision": "bbf4d82f2506f627858f",
+    "url": "/moviedb/static/js/runtime-main.905bda44.js"
+  },
+  {
+    "revision": "9679fa906515bb5de2ed742eb1299a36",
+    "url": "/moviedb/static/media/logo.9679fa90.svg"
+  }
+]);
